f01fe82e9616041d0744e9c9780d87210f6512d9
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2020-06-11 16:21:29 +0100

    Document Partage : CRUD Opretion Done Remark : Delete work in DB but Erreur Show Up & Delete File from wwwRoot doesn't work

--- StudentAPI/StudentAPI/AppService/Implementation/DocumentPartageAppService.cs
+++ StudentAPI/StudentAPI/AppService/Implementation/DocumentPartageAppService.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using StudentAPI.AppService.Contracts;
 using StudentAPI.Controllers.Resources.DocumentPartage;
 using StudentAPI.Core.IRepository;
 using StudentAPI.Core.Models;
 using StudentAPI.Core.QueryObject;
+using System.IO;
 
 namespace StudentAPI.AppService.Implementation
 {
@@ -12,12 +14,30 @@ namespace StudentAPI.AppService.Implementation
         private readonly IMapper _mapper;
         private readonly IDocumentPartageRepository _repository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<DocumentFile> _genericRepository;
+        private readonly IHostingEnvironment _host;
 
-        public DocumentPartageAppService(IMapper mapper, IDocumentPartageRepository repository, IUnitOfWork unitOfWork) : base(mapper, repository, unitOfWork)
+        public DocumentPartageAppService(IMapper mapper, IDocumentPartageRepository repository, IUnitOfWork unitOfWork, IGenericRepository<DocumentFile> genericRepository, IHostingEnvironment host) : base(mapper, repository, unitOfWork)
         {
             _mapper = mapper;
             _repository = repository;
             _unitOfWork = unitOfWork;
+            _genericRepository = genericRepository;
+            _host = host;
+        }
+
+        public async void RemoveWithFile(int id)
+        {
+            var document = (await _repository.GetById(id)).Document;
+            _genericRepository.Remove(document.Id);
+
+            var documentFullPath = _host.WebRootPath + "/Documents/" + document.FileName;
+
+            if (File.Exists(documentFullPath))
+                File.Delete(documentFullPath);
+
+            _repository.Remove(id);
+            await _unitOfWork.CompleteAsync();
         }
     }
 }
